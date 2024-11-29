04e88405a151645820caef8720bb13ecfcf1eab7
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2024-11-30 00:26:22 +0530

    2 Day

--- Java Basics/Basics.java
+++ Java Basics/Basics.java
@@ -1,1 +1,7 @@
 // basics of java like operators, array, datatypes etc... !!
+
+class basics{
+    public static void main(String[] args) {
+        System.out.println("now we are start to learn about JAVA SPRINGBOOT framework OK !!");
+    }
+}
