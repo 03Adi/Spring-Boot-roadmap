f5b539947511c0636c514bf6727935d2b2849db2
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2024-12-04 00:18:19 +0530

    3rd Dec

---
+++ Java Basics/DataTypes/datatypes.java
@@ -0,0 +1,26 @@
+package DataTypes;
+
+public class datatypes {
+    public static void main(String... arg){
+
+        //Data Types ---
+        //1. Primitive Data Types
+        System.out.println("Byte: 8");
+        System.out.println("Short: 16");
+        System.out.println("Int: 32");
+        System.out.println("Long: 64");
+        System.out.println("float: 32");
+        System.out.println("double: 64");
+        System.out.println("char: 16");
+        System.out.println("Boolean: 1");
+
+        //2. Non-Primitive Data Types
+
+        //Class
+        //Package
+        //Array
+        //Abstract
+        //Interface
+        //String
+    }
+}
