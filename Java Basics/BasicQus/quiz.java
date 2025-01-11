d39d09be9615aa3375f1d4cbcddc74024709f092
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2025-01-11 23:42:58 +0530

    Create quiz.java

---
+++ Java Basics/BasicQus/quiz.java
@@ -0,0 +1,10 @@
+package BasicQus;
+
+public class quiz {
+    public static void main(String arg[]){
+        int x = 5;
+        int y = 10;
+
+        System.out.println(x + y + " is the result ");
+    }
+}
